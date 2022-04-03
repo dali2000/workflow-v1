c5239c01cd46101e05c68e65c05b023ba19a7d13
dali2000 <medalikebair@gmail.com>
2022-04-03 10:36:49 +0100

    add user

--- backend/workflow/src/user/user.controller.ts
+++ backend/workflow/src/user/user.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { userDTO } from './user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor (private UserService: UserService) {
+
+    }
+    @Get('users')  // http://localhost:3000/user
+    getUsers(){
+        return this.UserService.showAll();
+    }
+    @Post('addUser')         // http://localhost:3000/user
+    addUser(@Body() data:userDTO){
+        return this.UserService.create(data);
+    }
+    @Get('getUser/:id')       // http://localhost:3000/user/getUser/1
+    getUser(@Param('id') id:string){
+        return this.UserService.showOne(id);
+    }
+    @Put('updateUser/:id')      // http://localhost:3000/user/updateUser/1
+    updateUser(@Param('id') id:string, @Body() data:Partial<userDTO>){
+        return this.UserService.update(id, data);
+    }
+    @Delete('deleteUser/:id')    // http://localhost:3000/user/deleteUser/1
+    deleteUser(@Param('id') id:string){
+        return this.UserService.destroy(id);
+    }
+}
--- backend/workflow/src/user/user.service.ts
+++ backend/workflow/src/user/user.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { userDTO } from './user.dto';
+import { User } from './user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    constructor(
+        @InjectRepository(User)
+        private userRepository: Repository<User>,
+
+    ){}
+    async showAll(){
+        return await this.userRepository.find()
+    }
+    async showOne(id: string){
+        return await this.userRepository.findOne({where: {id}});
+    }
+    async create(data:userDTO){
+        const user = await this.userRepository.create(data);
+        await this.userRepository.save(user);
+        return user;
+    }
+    async update(id: string, data:Partial<userDTO>){
+        await this.userRepository.update(id, data);
+        return this.userRepository.findOne(id);
+    }
+    async destroy(id: string){
+        await this.userRepository.delete(id);
+        return {deleted: true};
+    }
+}
